a3997771068d7e12f83f539702a8d2ccc3d2e6c6
disposable-kiwi <biffboffkev@gmail.com>
2022-08-24 18:01:17 -0400

    making the messaging route for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,21 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def index
+        @all_articles = Article.all
+    end
+
+    def new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
 end
